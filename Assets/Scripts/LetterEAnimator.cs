35d8af695abe6385bdc07f4479a07d1003b9d01d
ashamy97 <ashamy@mcmaster.ca>
2023-04-01 19:02:35 -0400

    created letters A to M

---
+++ Assets/Scripts/LetterEAnimator.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterEAnimator : MonoBehaviour
+{
+    private float animationDuration = 20.0f;
+
+    private LineRenderer lineRenderer, secondSlashRenderer, thirdSlashRenderer;
+    private Vector3[] linePoints;
+    private int pointsCount;
+    public GameObject secondSlash, thirdSlash;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        secondSlashRenderer = secondSlash.GetComponent<LineRenderer>();
+        thirdSlashRenderer = thirdSlash.GetComponent<LineRenderer>();
+
+        //store copies of linerenderer points
+        pointsCount = lineRenderer.positionCount;
+        linePoints = new Vector3[pointsCount];
+        for (int i = 0; i < pointsCount; i++)
+        {
+            linePoints[i] = lineRenderer.GetPosition(i);
+        }
+
+        StartCoroutine(AnimateLine());
+        StartCoroutine(AnimateSecondSlash());
+        StartCoroutine(AnimateThirdSlash());
+    }
+
+    private IEnumerator AnimateLine()
+    {
+        float segmentDuration = animationDuration / pointsCount;
+
+        for (int i = 0; i < pointsCount - 1; i++)
+        {
+            float startTime = Time.time;
+
+            Vector3 startPosition = linePoints[i];
+            Vector3 endPosition = linePoints[i + 1];
+
+            Vector3 pos = startPosition;
+
+            while (pos != endPosition)
+            {
+                float t = (Time.time - startTime) / segmentDuration;
+                pos = Vector3.Lerp(pos, endPosition, t);
+
+                for (int j = i + 1; j < pointsCount; j++)
+                {
+                    lineRenderer.SetPosition(j, pos);
+                }
+
+                yield return null;
+            }
+        }
+
+    }
+
+    private IEnumerator AnimateSecondSlash()
+    {
+        secondSlash.SetActive(false);
+        yield return new WaitForSeconds(1.75f);
+        secondSlash.SetActive(true);
+
+        float startTime = Time.time;
+
+        Vector3 startPosition = secondSlashRenderer.GetPosition(0);
+        Vector3 endPosition = secondSlashRenderer.GetPosition(1);
+
+        Vector3 pos = startPosition;
+
+        while (pos != endPosition)
+        {
+
+            float t = (Time.time - startTime) / animationDuration;
+            pos = Vector3.Lerp(pos, endPosition, t);
+
+            secondSlashRenderer.SetPosition(1, pos);
+
+            yield return null;
+        }
+
+
+    }
+
+    private IEnumerator AnimateThirdSlash()
+    {
+        thirdSlash.SetActive(false);
+        yield return new WaitForSeconds(3.1f);
+        thirdSlash.SetActive(true);
+
+        float startTime = Time.time;
+
+        Vector3 startPosition = thirdSlashRenderer.GetPosition(0);
+        Vector3 endPosition = thirdSlashRenderer.GetPosition(1);
+
+        Vector3 pos = startPosition;
+
+        while (pos != endPosition)
+        {
+
+            float t = (Time.time - startTime) / animationDuration;
+            pos = Vector3.Lerp(pos, endPosition, t);
+
+            thirdSlashRenderer.SetPosition(1, pos);
+
+            yield return null;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/LineAnimator2.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineAnimator2 : MonoBehaviour
+{
+    private float animationDuration = 20.0f;
+
+    private LineRenderer lineRenderer;
+    private Vector3[] linePoints;
+    private int pointsCount;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        
+
+        //store copies of linerenderer points
+        pointsCount = lineRenderer.positionCount;
+        linePoints = new Vector3[pointsCount];
+        for (int i = 0; i < pointsCount; i++)
+        {
+            linePoints[i] = lineRenderer.GetPosition(i);
+        }
+
+        StartCoroutine(AnimateLine());
+        
+    }
+
+    private IEnumerator AnimateLine()
+    {
+        float segmentDuration = animationDuration / pointsCount;
+
+        for (int i = 0; i < pointsCount - 1; i++)
+        {
+            float startTime = Time.time;
+
+            Vector3 startPosition = linePoints[i];
+            Vector3 endPosition = linePoints[i + 1];
+
+            Vector3 pos = startPosition;
+
+            while (pos != endPosition)
+            {
+                float t = (Time.time - startTime) / segmentDuration;
+                pos = Vector3.Lerp(pos, endPosition, t);
+
+                for (int j = i + 1; j < pointsCount; j++)
+                {
+                    lineRenderer.SetPosition(j, pos);
+                }
+
+                yield return null;
+            }
+        }
+
+    }
+}
